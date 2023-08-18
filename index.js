3f9d2b124738514294bfffaaa95c4f6694a324af
Alexei Ivanov <alekseyivanoof34@gmail.com>
2023-08-19 00:18:23 +0700

    дз 2 в процессе разработки

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 import { renderPage } from "./game-area";
 
+export function difficultPage() {
+    const  pageElement = document.getElementById("app")
+    const pageDifficult = 
+    `<div  class="game-cart">
+    <div class="game-menu center" >
+<h2 class="game-set">Выбери<br>сложность</h2>
+<div class="game-difficult">
+    <button class="game-difficult-numb" id="level-easy">1</button>
+    <button class="game-difficult-numb" id="level-medium">2</button>
+    <button class="game-difficult-numb" id="level-hard">3</button>
+</div>
+<button id="start-game" class="game-button">Старт</button>
+    </div>
+</div>`
+pageElement.innerHTML = pageDifficult.Html;
+
+
 const easyGame = document.getElementById("level-easy");
 const mediumGame = document.getElementById("level-medium");
 const hardGame = document.getElementById("level-hard");
 const startGame = document.getElementById("start-game");
-
 
 easyGame.addEventListener("click", () => {
     alert ("Вы выбрали Легкий уровень игры, нажмите Старт для продолжения ")
@@ -19,5 +35,10 @@ hardGame.addEventListener("click", () => {
 })
 
 startGame.addEventListener("click", () => {
-    return renderPage
+    alert("Старт!")
+    return  renderPage();
 })
+   
+};
+
+difficultPage();
